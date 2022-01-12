a4bf8c1ca9f11b41df6b49e14f2af5bc11f99f44
Chinseok Lee <me@askcompany.kr>
2022-01-12 10:10:32 +0900

    news 앱 생성 및 등록

--- myproj/urls.py
+++ myproj/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('news/', include('news.urls')),
     path('shop/', include("shop.urls")),
 ]
