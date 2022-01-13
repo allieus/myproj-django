dff957e53775a8e79fcea3bf9263495221327eb0
Chinseok Lee <me@askcompany.kr>
2022-01-13 10:26:23 +0900

    news.Article 모델에 photo 필드 추가 및 마이그레이션 파일 생성

--- news/models.py
+++ news/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
+    photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
